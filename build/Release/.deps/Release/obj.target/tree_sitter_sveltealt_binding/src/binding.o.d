d16c4b74df8a8e8cfb13a94ca2eb75f4061dc93b
Himujjal <himu@tuta.io>
2021-03-26 04:23:05 +0530

    fixed Cargo.toml

---
+++ build/Release/.deps/Release/obj.target/tree_sitter_sveltealt_binding/src/binding.o.d
@@ -0,0 +1,64 @@
+cmd_Release/obj.target/tree_sitter_sveltealt_binding/src/binding.o := g++ '-DNODE_GYP_MODULE_NAME=tree_sitter_sveltealt_binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/himu/.cache/node-gyp/10.19.0/include/node -I/home/himu/.cache/node-gyp/10.19.0/src -I/home/himu/.cache/node-gyp/10.19.0/deps/openssl/config -I/home/himu/.cache/node-gyp/10.19.0/deps/openssl/openssl/include -I/home/himu/.cache/node-gyp/10.19.0/deps/uv/include -I/home/himu/.cache/node-gyp/10.19.0/deps/zlib -I/home/himu/.cache/node-gyp/10.19.0/deps/v8/include -I../node_modules/nan -I../src  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -fPIC -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/tree_sitter_sveltealt_binding/src/binding.o.d.raw   -c -o Release/obj.target/tree_sitter_sveltealt_binding/src/binding.o ../src/binding.cc
+Release/obj.target/tree_sitter_sveltealt_binding/src/binding.o: \
+ ../src/binding.cc ../src/tree_sitter/parser.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/node.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/v8.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/v8-version.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/v8config.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/v8-platform.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/node_version.h \
+ ../node_modules/nan/nan.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/node_version.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/uv.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/uv/errno.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/uv/version.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/uv/unix.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/uv/threadpool.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/uv/linux.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/node_buffer.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/node.h \
+ /home/himu/.cache/node-gyp/10.19.0/include/node/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_private.h \
+ ../node_modules/nan/nan_typedarray_contents.h \
+ ../node_modules/nan/nan_json.h
+../src/binding.cc:
+../src/tree_sitter/parser.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/node.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/v8.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/v8-version.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/v8config.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/v8-platform.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/node_version.h:
+../node_modules/nan/nan.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/node_version.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/uv.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/uv/errno.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/uv/version.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/uv/unix.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/uv/threadpool.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/uv/linux.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/node_buffer.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/node.h:
+/home/himu/.cache/node-gyp/10.19.0/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_private.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../node_modules/nan/nan_json.h:
---
+++ build/Release/.deps/Release/obj.target/tree_sitter_sveltealt_binding/src/parser.o.d
@@ -0,0 +1,5 @@
+cmd_Release/obj.target/tree_sitter_sveltealt_binding/src/parser.o := cc '-DNODE_GYP_MODULE_NAME=tree_sitter_sveltealt_binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/himu/.cache/node-gyp/10.19.0/include/node -I/home/himu/.cache/node-gyp/10.19.0/src -I/home/himu/.cache/node-gyp/10.19.0/deps/openssl/config -I/home/himu/.cache/node-gyp/10.19.0/deps/openssl/openssl/include -I/home/himu/.cache/node-gyp/10.19.0/deps/uv/include -I/home/himu/.cache/node-gyp/10.19.0/deps/zlib -I/home/himu/.cache/node-gyp/10.19.0/deps/v8/include -I../node_modules/nan -I../src  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -fPIC -O3 -fno-omit-frame-pointer -std=c99 -MMD -MF ./Release/.deps/Release/obj.target/tree_sitter_sveltealt_binding/src/parser.o.d.raw   -c -o Release/obj.target/tree_sitter_sveltealt_binding/src/parser.o ../src/parser.c
+Release/obj.target/tree_sitter_sveltealt_binding/src/parser.o: \
+ ../src/parser.c ../src/tree_sitter/parser.h
+../src/parser.c:
+../src/tree_sitter/parser.h:
